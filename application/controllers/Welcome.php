b3e69f95ee205f39f5dda28c39b179a337794aa9
edwinqzolve <edwinqzolve@gmail.com>
2020-04-25 15:28:29 +0530

    user information

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -12,15 +12,15 @@ function __construct()
         $this->load->model('Welcome_Model'); 
          
     }
-	
-	public function index()
-	{
-		
+  
+  public function index()
+  {
+    
         $data['st'] =$this->test();
         $data['title'] = "Process Update";
         $this->load->view('pro', $data);
-	}
-	public function test(){
+  }
+  public function test(){
          
           $view_data["to_do"] = $this->Welcome_Model->get_statusto_do();
           $view_data["in_progress"] = $this->Welcome_Model->get_statusin_progress();
@@ -30,21 +30,82 @@ function __construct()
 
 
 
-	}
+  }
   public function employee()
   {
     
         $data['title'] = "Process Update";
         $this->load->view('employee_crud/employee_details', $data);
   }
-
+     /*******************************************************************************
+     * Author : Bincy                                                              *
+     * Detail :View page for User Information 
+     * Date   : 24-04-2020                                                         *
+     *******************************************************************************/
     public function crud()
   {
     
-        $data['title'] = "Survey Responses";
+        $data['title'] = "User Details";
         $this->load->view('skeleton_view', $data);
   }
+     /*******************************************************************************
+     * Author : Bincy                                                              *
+     * Detail : Table listing User Information 
+     * Date   : 24-04-2020                                                         *
+     *******************************************************************************/
 
+  public function userdetails_lists()
+  {
+   $list = $this->Welcome_Model->get_userdetaillist_datatable();
+        $data = array(); 
+        $no = $_POST['start'];
+        foreach ($list as $user_detail) {
+            $no++;
+            $row   = array();
+            $row[] = $no;
+            $row[] = $user_detail->cust_type;
+            $row[] = $user_detail->cust_name;
+            $row[] = $user_detail->cust_add1;
+            $row[] = $user_detail->cust_add2;
+            $row[] = $user_detail->cust_country;
+            $row[] = $user_detail->cust_city;
+            $row[] = $user_detail->cust_region;           
+            $row[] = $user_detail->cust_zip;
+            $row[] = $user_detail->cust_email;
+            $row[] = $user_detail->cust_officephone;
+            $row[] = $user_detail->cust_mobile;
+            $row[] = $user_detail->cust_fax;
+            $row[] = $user_detail->cust_website;                     
+            $row[] = ' <a href="javascript:void(0);" ng-click="EditModal(user);"> 
+                <i class="glyphicon glyphicon-pencil">EDIT</i>
+              </a>
+              <a href="javascript:void(0);" ng-click="DeleteModal(user)" class="delete"> 
+                <i class="glyphicon glyphicon-remove">DELETE</i> 
+              </a>';     
+           $data[] = $row;
+        }
+
+        $output = array(
+                    "draw"            => $_POST['draw'],
+                    "recordsTotal"    => $this->Welcome_Model->count_alls(),
+                        "recordsFiltered" => $this->Welcome_Model->count_filtered(),
+                    "data" => $data,
+                );
+        echo json_encode($output);
+  }
+
+     /*******************************************************************************
+     * Author : Bincy                                                              *
+     * Detail : Save table for User Information 
+     * Date   : 24-04-2020                                                         *
+     *******************************************************************************/
+  public function costchildsave()
+  {
+    $data=$this->Welcome_Model->Userdetailsave();
+    echo json_encode($data);
+  }
+
+  
   public function get_users()
   {
      
@@ -67,7 +128,6 @@ function __construct()
 
 
   }
-
 
   
 }
