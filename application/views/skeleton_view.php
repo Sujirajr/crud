81a4411453bfa0eae4af6163abb0c661a59ccaf0
sujiraj93 <sujiraj93@gmail.com>
2020-04-27 05:43:52 +0530

    file changes

--- application/views/skeleton_view.php
+++ application/views/skeleton_view.php
@@ -116,12 +116,12 @@
 <div class="kt-portlet__head">
 <div class="kt-portlet__head-label">
 <h3 class="kt-portlet__head-title">
-3 Columns Form Layout
+Add New User
 </h3>
 </div>
 </div>
 <!--begin::Form-->
-<form class="kt-form kt-form--label-right">
+<form class="kt-form kt-form--label-right" id="user-form">
 <div class="kt-portlet__body">
 <div class="form-group row">
 <div class="col-lg-4">
@@ -208,7 +208,8 @@
 <div class="row">
 <div class="col-lg-4"></div>
 <div class="col-lg-8">
-<button type="submit" class="btn btn-primary float-right mr-2" id="Customerdetail_submit">Submit</button>
+				<input class="submit btn btn-primary float-right mr-2" id="Customerdetail_submit" type="submit" value="Login">
+<!-- <button type="submit" class="submit btn btn-primary float-right mr-2" id="Customerdetail_submit" value="Login">Submit</button> -->
 
 <button type="reset" class="btn btn-secondary float-right mr-2">Cancel</button></div>
 <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
@@ -283,12 +284,42 @@
 							<!-- begin:: Main Content -->
 
 							<!-- End:: Main Content -->
-						
+				
+
+		
+
+
+
+
+
+
 
 						<?php  $this->load->view("common/footer"); ?>
 
+	<script src="<?php echo back_skin; ?>assets/lib/jquery.mockjax.js"></script>
+	<script src="<?php echo back_skin; ?>assets/lib/jquery.form.js"></script>
+	<script src="<?php echo back_skin; ?>assets/dist/jquery.validate.js"></script>
+
+
 <script src="<?php echo back_skin; ?>assets/customjs/usersdetails.js"></script>
 <script src="<?php echo back_skin; ?>assets/plugins/custom/datatables/jquery.dataTables.min.js"></script>
+
+
+
+
+<!------->
+
+<script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.flash.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/select/1.3.1/js/dataTables.select.min.js "></script>
+
+<!------->
+
 <script src="<?php echo back_skin; ?>assets/js/pages/components/extended/sweetalert2.js" type="text/javascript"></script>
 
 <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js" type="text/javascript"></script>
